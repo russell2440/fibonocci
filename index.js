a3b4a6035790a5d7093ef0e18e5b519dce2cdfad
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-09-07 04:35:56 +0000

    finished?

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 // returns fibonocci number associated with index
 //
 function fibonocci_recursive(index){
-
+  if(index < 2) {
+    return index;
+  }
+  return fibonocci_recursive(index-1)+fibonocci_recursive(index-2);
 }
 
-let num = fibonocci_recursive(8);
-console.log("fibrecurse: "+num);
+fibonocci_recursive(8);
+
+for (let i = 0; i++; i<10){
+  console.log("fibrecurse: "+i+", "
+    +fibonocci_recursive(i));
+}
