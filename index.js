eeb61d87ed30fefbd2ad6ebcb2ff3a68b22fb67f
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-09-09 16:18:33 +0000

    added iterative approach

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ function fibonocci_recursive(index){
   return fibonocci_recursive(index-1)+fibonocci_recursive(index-2);
 }
 
+function fibonocci_iterative(index){
+  let arr = [0,1];
+  for(let i = 2; i < index+1; i++) {
+    arr.push(arr[i-2]+arr[i-1]);
+  }
+  return arr[index];
+}
+
 
 for (let i = 0; i<10; i++){
   console.log("fibrecurse: "+i+", "
     +fibonocci_recursive(i));
+}
+console.log("=============================")
+for (let i = 0; i<10; i++){
+  console.log("fibrecurse: "+i+", "
+    +fibonocci_iterative(i));
 }
